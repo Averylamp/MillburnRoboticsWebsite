443a4beb5dd918d6b11e8bc1aad3fc6cf2b42a98
Ryan Sullivan <rps214@verizon.net>
2015-05-20 15:01:34 -0400

    playing around with javascript

---
+++ js/advert.js
@@ -0,0 +1,11 @@
+//This is just scripting practice, feel free to delete this file
+
+
+setInterval(function(){flash()}, 2000);
+document.querySelector("#advertText").style.display = "inline-block";
+document.querySelector("#advertText").style.marginBottom = 100;
+document.querySelector("#advert").style.color.margin = "20";
+function flash(){
+    document.querySelector("#advertText").style.color = "#ff0000";
+    setTimeout(function(){document.querySelector("#advertText").style.color = "#00000F";}, 1000);
+}
