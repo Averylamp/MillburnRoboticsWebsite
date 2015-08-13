0d1ac2ca3185efde9cd98164577b44e25d781896
Shannon Peng <shannonpeng98@gmail.com>
2015-08-13 00:44:53 -0400

    Make mobile friendly

--- draft/js/main.js
+++ draft/js/main.js
@@ -3,8 +3,23 @@ $(window).load(
 
 		$(window).scrollTop(0);
 
-		particlesJS.load('header', 'assets/particles.json', function() {});
+		$(window).resize(function() {
+			$('#nav ul.links li, #nav ul.social').removeAttr('style');
+		});
 
-	}
+    }
+
 );
+
+var toggleNav = function(x, open) {
+    if (open) {
+        $('#nav ul.links li, #nav ul.social').css('display', 'none');
+        x.setAttribute('onclick', "javascript:toggleNav(this, false);")
+    }
+		
+	else {
+    	$('#nav ul.links li, #nav ul.social').css('display', 'block');
+    	x.setAttribute('onclick', "javascript:toggleNav(this, true);")
+	}
+}
 
